6e5a8a2bfd33ad0384e6d8b986071a3e8e3cf131
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-08 18:51:42 -0500

    Adds class TextFile which extends the File class -	Adds declarations for constructors and accessor functions.s

---
+++ Source/TextFile.hpp
@@ -0,0 +1,23 @@
+﻿#ifndef TEXTFILE_HPP
+#define TEXTFILE_HPP
+
+#include "File.hpp"
+#include "SafeArray.hpp"
+#include <string>
+
+class TextFile : public File{
+public:
+	TextFile ( );
+
+	TextFile (std::string fileName);
+	
+	int getSize ( ) const override;
+
+	int getCharCount ( ) const;
+private:
+	SafeArray<int> charList;
+	int charCount;
+};
+
+#endif //TEXTFILE_HPP
+
