5781f960b007bdfcf7d56bf73e1e478f6404208a
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-08 01:35:13 -0500

    Implements both constructors, getName and getExtension of File

---
+++ Source/File.cpp
@@ -0,0 +1,21 @@
+﻿// File.cpp
+// Created by Evan Almonte
+//
+#include "File.hpp"
+#include <string>
+
+using std::string;
+
+File::File( ) : name(""), extension(""), size(0) { }
+
+File::File(std::string name, std::string extension) : name(name), extension(extension), size(0) { }
+
+File::~File( ) {}
+
+string File::getName( ) const {
+	return name;
+}
+
+string File::getExtension( ) const {
+	return extension;
+}
