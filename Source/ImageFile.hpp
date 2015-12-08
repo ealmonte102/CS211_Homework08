306307ab1dddb17dd98bf8455c9494f44b2584ec
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-08 02:23:47 -0500

    Adds class ImageFile which extends from class File
    -	Adds the declaration for various member functions.

---
+++ Source/ImageFile.hpp
@@ -0,0 +1,26 @@
+﻿#ifndef IMAGEFILE_HPP
+#define IMAGEFILE_HPP
+
+#include "File.hpp"
+#include "SafeMatrix.hpp"
+#include <string>
+
+class ImageFile : File {
+public:
+	ImageFile( );
+
+	ImageFile(std::string fileName);
+
+	int getColorDepth( ) const;
+
+	int getDimensionHeight( ) const;
+
+	int getDimensionWidth( ) const;
+
+	int getSize( ) const;
+private:
+	SafeMatrix<int> pixelMatrix;
+	int colorDepth;
+};
+
+#endif //IMAGEFILE_HPP
