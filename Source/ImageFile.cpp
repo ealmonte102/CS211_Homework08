e6928f927175d366ff7f062c444613172a2156e1
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-08 02:46:25 -0500

    Implements both constructors of ImageFile

---
+++ Source/ImageFile.cpp
@@ -0,0 +1,9 @@
+﻿// ImageFile.cpp
+// Created by Evan Almonte
+//
+#include "ImageFile.hpp"
+
+ImageFile::ImageFile( ) : pixelMatrix(SafeMatrix<int>(0,0)), colorDepth(0) { }
+
+ImageFile::ImageFile(std::string fileName, int height, int width, int cD) : 
+	File(fileName, ".gif"), pixelMatrix(SafeMatrix<int>(height,width)), colorDepth(cD) { }
