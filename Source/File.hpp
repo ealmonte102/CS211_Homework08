6027cd4fba91abb11c9300938318b64d3816f174
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-08 01:20:52 -0500

    Creates pure virtual class File

---
+++ Source/File.hpp
@@ -0,0 +1,24 @@
+﻿#ifndef FILE_HPP
+#define FILE_HPP
+#include <string>
+
+class File {
+public:
+	File ( );
+	
+	File (std::string name, std::string extenstion);
+	
+	virtual ~File ( );
+	
+	std::string getName ( ) const;
+	
+	std::string getExtension ( ) const;
+	
+	virtual int getSize ( ) const = 0;
+private:
+	std::string name;
+	std::string extension;
+	int size;
+};
+
+#endif //FILE_HPP
