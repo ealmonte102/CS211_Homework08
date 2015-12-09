33bc094f26bfa2029c9085931db1a661968c6555
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-08 19:07:08 -0500

    Modifies TextFile's constructors to properly init. parent's members

--- Source/TextFile.cpp
+++ Source/TextFile.cpp
@@ -3,9 +3,9 @@
 //
 #include "TextFile.hpp"
 
-TextFile::TextFile( ) : charList(SafeArray<int>(0)), charCount(0) { }
+TextFile::TextFile( ) : File("",".txt"), charList(SafeArray<int>(0)), charCount(0) { }
 
-TextFile::TextFile(std::string fileName) : charList(SafeArray<int>(0)), charCount(0) { }
+TextFile::TextFile(std::string fileName) : File(fileName, ".txt"), charList(SafeArray<int>(0)), charCount(0) { }
 
 TextFile::TextFile(std::string fileName, SafeArray<int> charList) : File(fileName, ".txt"), charList(charList), charCount(charList.length()) {}
 
