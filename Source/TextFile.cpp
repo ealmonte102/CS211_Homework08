be971fc267911fb2c9ddcafacbfa10e0917e376d
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-08 19:00:53 -0500

    Implements all functions of the TextFile class

---
+++ Source/TextFile.cpp
@@ -0,0 +1,16 @@
+﻿// TextFile.cpp
+// Created by Evan Almonte
+//
+#include "TextFile.hpp"
+
+TextFile::TextFile( ) : charList(SafeArray<int>(0)), charCount(0) { }
+
+TextFile::TextFile(std::string fileName) : charList(SafeArray<int>(0)), charCount(0) { }
+
+int TextFile::getSize() const {
+	return charCount / 8;
+}
+
+int TextFile::getCharCount() const {
+	return charCount;
+}
